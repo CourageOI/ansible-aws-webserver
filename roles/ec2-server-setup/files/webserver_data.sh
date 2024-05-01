b019e59e3cfa931adb9ce1af88a3fb0f805d637c
Osakpolor Ihensekhien <osakpolor.ihensekhien@kuda.com>
2024-05-01 13:02:05 +0100

    update playbook and roles

--- roles/ec2-server-setup/files/webserver_data.sh
+++ roles/ec2-server-setup/files/webserver_data.sh
@@ -4,8 +4,8 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-# Get the public IP address
-public_ip=$(hostname -I | awk '{print $1}')
+# Get the local IP address
+local_ip=$(hostname -I | awk '{print $1}')
 
 # Create a simple index.html file with the IP address
-echo "<html><head><title>My Web Server</title></head><body><h1>Hello from EC2 Instance!</h1><p>Public IP: $public_ip</p></body></html>" > /var/www/html/index.html
+echo "<html><head><title>My Web Server</title></head><body><h1>Hello from EC2 Instance!</h1><p>Local IP: $local_ip</p></body></html>" > /var/www/html/index.html
